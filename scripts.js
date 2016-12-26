2b431d7ecd97a2e670fa108e80bb7dc7545e4046
Tee Mak <Tee Mak>
2016-12-26 10:31:45 -0500

    Completed exercises

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 //Use this file to implement Part One of your project
 
+var animal = {};
 
+animal.username = 'Tee';
+animal['tagline'] = 'Sup!';
+
+var noises = [];
+animal.noises = noises;
+
+console.log(animal);
+
+var counter = 0;
+var loop = function(object) {
+    
+    for(var key in object) {
+      if(key === 'username') {
+        console.log(`Hi my name is ${animal[key]}`);
+      } else {
+        console.log(`I like to say ${animal[key]}!`);
+      }
+      counter++;    
+    }
+    
+};
